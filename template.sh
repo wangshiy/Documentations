2f6be84aaeec02b0ac1bfd8a822b51b49815cebf
Shiyu Wang <shiyu.wang1@huawei.com>
2017-10-03 14:34:48 -0700

    init a shell template generating template

---
+++ template.sh
@@ -0,0 +1,9 @@
+read -p "What is the index?" n 
+read -p "What is the question number on leetcode?" i
+read -p "What is the question url?" url
+read -p "What is the title for this question?" title
+read -p "What is the content for this question?" content
+read -p "What is the filename to insert the template?" filename
+result="#### $n. [Leetcode#$i]($url) $title ?\n- $content\n\`\`\`javascript\n\`\`\`"
+echo $result >> $filename
+echo "succeed!"
